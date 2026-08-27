3685649661b346361c4545454686421feb3a213b
jatin110206 <jatinprakashrathore@gmail.com>
2026-08-27 15:59:41 +0530

    feat: configure Vercel deployment settings for backend API and frontend routing

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,36 @@
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
-const express=require('express');
-const cors=require('cors');
-const app=express();
-const connectToDb=require('./db/db');
-const userRoutes=require('./routes/user.routes');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
-const captainRoutes=require('./routes/captain.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    res.send('hello world');
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'online',
+    service: 'RathoreTaxi API',
+    message: 'Backend server is running successfully',
+  });
 });
 
-app.use('/users',userRoutes);
+app.use('/users', userRoutes);
+app.use('/captain', captainRoutes);
 
-app.use('/captain',captainRoutes);
-
-
-module.exports=app;
+module.exports = app;
